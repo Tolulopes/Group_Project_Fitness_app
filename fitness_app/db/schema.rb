1578752918655058bd9371bacf2428d8f7eb40e5
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-02 14:27:16 +0000

    review model created and migrated

--- fitness_app/db/schema.rb
+++ fitness_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302141810) do
+ActiveRecord::Schema.define(version: 20150302142646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20150302141810) do
     t.integer  "instructor_id"
     t.integer  "capacity"
     t.integer  "category_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "lesson_id"
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
