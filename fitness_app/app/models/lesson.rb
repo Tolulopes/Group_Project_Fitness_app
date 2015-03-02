853dd042bbb634ff6adf3ab3a4a3a1b810abfe0e
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-02 14:24:38 +0000

    category model created and association with lessons established

--- fitness_app/app/models/lesson.rb
+++ fitness_app/app/models/lesson.rb
@@ -2,4 +2,5 @@ class Lesson < ActiveRecord::Base
   belongs_to :instructor, -> { where role: "instructor" }, class_name: "User"
   has_many :bookings
   has_many :users, through: :bookings
+  belongs_to :category
 end
