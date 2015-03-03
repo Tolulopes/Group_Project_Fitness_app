4aaf996f402ca9f6751a68f6f70781c508daaf5e
Oliver Cairns <olicairns93@gmail.com>
2015-03-03 15:00:44 +0000

    basic form for writing new reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,39 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    @user = current_user
+  end
+
+  def new
+    @review = Review.new
     @lessons = Lesson.all
   end
 
+  def create
+    @review = Review.create(review_params)
+    @review.user_id = current_user.id
+    @review.save
+    redirect_to reviews_path
+  end
+
+  def edit
+    @review = review.find(params[:id])
+    @categories = Lesson.all
+  end
+
+  def update
+    @review = review.find(params[:id])
+    @review.update(review_params)
+    redirect_to reviews_path
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :comment, :lesson_id, :capacity)
+  end
 end
