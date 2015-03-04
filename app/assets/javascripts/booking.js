56d762f98ee2d1f767e858124d6c5e83d57a6049
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-04 21:13:07 +0000

    progress on ajax-ing instructor confirming booking on button click

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -21,6 +21,20 @@ function createBooking(lesson, user, stat){
   });
 };
 
+function confirmBooking(lesson, user, stat){
+  console.log("inside confirm booking function")
+  
+
+  // request("POST", "/bookings", {booking:{lesson_id: lesson, user_id: user, status: stat}}).done(function(data){
+  //   console.log("updated booking table, ", "lesson id: ", lesson, "user id: ", user, stat);
+  //   $("#apply-to-join").hide();
+
+  //   var nowAvailable = $("#lesson-availability").data("avail") - 1;
+
+  //   $("#lesson-availability").replaceWith("<li>Available space left: " + nowAvailable + "</li>");
+  // });
+};
+
 $(document).ready(function(){
   $("#apply-to-join").on("click", function(e){
 
@@ -29,6 +43,27 @@ $(document).ready(function(){
     var stat = "pending";
 
     createBooking(lesson.toString(), user.toString(), stat);
+
+  });
+
+  $(".confirm-booking").on("click", function(e){
+
+    $(this).each(function(index, booking){
+      var lesson = $(booking).data('id');
+      var user = $(booking).data('user');
+      var stat = "confirmed";
+      
+      confirmBooking(lesson.toString(), user.toString(), stat);
+
+    });
+
+    // var lesson = $(".confirm-booking").data('id');
+    // var user = $(".confirm-booking").data('user');
+    // var stat = "confirmed";
+
+    // console.log(lesson, user, stat);
+
+    // confirmBooking();
 
   });
 
