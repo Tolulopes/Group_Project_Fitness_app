7f1af2ae25d312a234e287961499be146de647bf
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-04 15:21:01 +0000

    refactoring, added bookings viewable by logged-in instructory only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @lessons = Lesson.all
+    @bookings = Booking.all
+    @bookings_as_client = @bookings.where(user_id: @user.id)
+    
+    @lessons_as_instructor = @lessons.where(instructor_id: @user.id)
+
+    # @bookings_as_instructor = @bookings.where(instructor_id: @user.id)
+
   end
 
 end
