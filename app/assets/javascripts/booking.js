590d5df0cdbb4f864aa69b4eef5a8d3fb845db45
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-04 19:13:25 +0000

    lesson view page booking ajax bug fixes

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -14,11 +14,10 @@ function createBooking(lesson, user, stat){
   request("POST", "/bookings", {booking:{lesson_id: lesson, user_id: user, status: stat}}).done(function(data){
     console.log("updated booking table, ", "lesson id: ", lesson, "user id: ", user, stat);
     $("#apply-to-join").hide();
-    var newAvailbility = $("#lesson-availability").data("avail") - 1 ;
-    // console.log(variable);
-    $("#lesson-availability").replaceWith("<li>Availability:" + newAvailability + "</li>");
 
-    // console.log($("#lesson-availability").data("lesson-id"));
+    var nowAvailable = $("#lesson-availability").data("avail") - 1;
+
+    $("#lesson-availability").replaceWith("<li>Available space left: " + nowAvailable + "</li>");
   });
 };
 
